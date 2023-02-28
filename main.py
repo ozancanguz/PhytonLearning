f4ac6c11f261e8339c9ba773f6a403529843305b
Ozan <ozancangu24@gmail.com>
2023-02-28 17:08:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#user input
+#userText=input("Enter a to do:  ")
+#print(userText)
+
+#storing string inputs
+userPrompt="Enter a todo"
+todo1=input(userPrompt)
+todo2=input(userPrompt)
+todo3=input(userPrompt)
+
+todos=[todo1,todo2,todo3]
+print(todos)
